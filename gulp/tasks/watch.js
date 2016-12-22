a99c8842c18ce4732519da355e48524211ac1021
Sergio Gimeno <gimeno.sergio@gmail.com>
2016-12-22 20:05:39 +0100

    add ghooks and codestyle as pre-push hook

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,6 @@ var gulp = require('gulp')
 
 gulp.task('watch', ['browserSync'], function () {
   // Scripts are automatically watched and rebundled by Watchify inside Browserify task
-  // gulp.watch(config.scripts.src, ['lint']);
   gulp.watch(config.styles.src, ['styles'])
   gulp.watch(config.images.src, ['images'])
   gulp.watch(config.fonts.src, ['fonts'])
